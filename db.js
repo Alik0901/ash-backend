1a00bca4a9c5e59300be6d3f5ccd1dade6aaa52e
AshBuilder <ash@prah.dev>
2025-06-16 19:13:58 +0300

    1

--- db.js
+++ db.js
@@ -1,12 +1,25 @@
+// src/db.js
 import pkg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  // включаем TCP keep-alive, чтобы держать сокеты живыми
+  keepAlive: true,
+  // закрывать неактивные клиенты через 30 сек
+  idleTimeoutMillis: 30_000,
+  // максимум 5 сек на установление нового подключения
+  connectionTimeoutMillis: 5_000,
+});
+
+// Ловим «неожиданные» ошибки клиента
+pool.on('error', (err, client) => {
+  console.error('Unexpected PG client error', err);
 });
 
 export default pool;
