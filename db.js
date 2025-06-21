43d34dc1dd9d9106533e54e949ed43a45c772e38
AshBuilder <ash@prah.dev>
2025-06-21 15:32:27 +0300

    1

--- db.js
+++ db.js
@@ -7,17 +7,18 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  // Если вы подключаетесь к приватному endpoint:
-  // в .env установите PGSSLMODE=disable
-  // и здесь ssl=false
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   ssl: process.env.PGSSLMODE === 'require'
        ? { rejectUnauthorized: false }
        : false,
-  max: 5,                  // <= pool size
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 5000,
-  keepAlive: true          // включаем TCP keep-alive
+  max: 5,
+  idleTimeoutMillis: 30_000,
+  connectionTimeoutMillis: 5_000,
+  keepAlive: true
 });
 
 pool.on('error', err => {
